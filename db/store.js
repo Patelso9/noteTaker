fe02bc7ac06c6771820fad08190cb88e64c9a50a
Princess <sonali.patel.pkg@gmail.com>
2021-07-07 22:58:34 -0400

    backuunbroke part of store

--- db/store.js
+++ db/store.js
@@ -1,24 +1,35 @@
 // look up uuid (util.promisify)- use ID in database
 const fs = require("fs");
 const path = require("path");
-const ( v4: uuidv4 ) = require("uuid");
+const { v4: uuidv4 } = require("uuid");
+const util = require("util");
+// const db = require('./db.json');
 
-// const util = require("util");
-// const db = require('db.json');
-
-const readNotes = util.promisify(fs.readfile);
+const readNotes = util.promisify(fs.readFile);
 
 class Store {
-
-    getNotes () {
-        readNotes('db.json', { encoding: 'utf8' })
-        .then(notes => {
-            return JSON.parse(notes)
-        })
-        .catch(err => {
-            console.log("there are no notes", err)
-        })
+    
+    readAll(){
+        return readNotes( './db.json',  'utf8')
     };
+    
+    // writeNotes();
+    
+    // deleteNotes();
+
+
+    async getNotes () {
+        return this.readAll()
+        .then((notes) => {
+            let parsedNotes
+            try {parsedNotes = [].concat(JSON.parse(notes))}
+         catch (err) {
+            console.log("there are no notes", err);
+        }
+        console.log('api routes get /notes', notes)
+        return parsedNotes;    
+    
+    })};
     
     // addNote(note) {
     //     const {title, text} = note;
@@ -36,14 +47,11 @@ class Store {
     //         }});
     // }
 
-    // writeNotes();
-
-    // deleteNotes();
 
     
 }
 
-module.export = new Store;
+module.exports = new Store();
 
 // ------------------- STARTER CODE ------------------------//
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,9 +7,10 @@ const store = require('../db/store');
    router.get('/notes', (req, res) => {
     store.getNotes()
     .then((notes) => {
+        console.log('api routes get /notes', notes)
         return res.json(notes)
     })
-    // .catch((err) => res.status(500).json(err))
+    .catch((err) => res.status(500).json(err))
 })
     
     // router.post('/notes', (req, res) => {
